2f9d2127d82c97d1b50b64912c05577c624bd36f
Guillermo Sotelo <guillermo.sotelo@conexiogroup.com>
2023-01-10 19:31:28 +0100

    added barchart rotation

--- src/components/BarChart/index.js
+++ src/components/BarChart/index.js
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Bar } from 'react-chartjs-2'
+import rotateIcon from '../../assets/rotate-icon.svg'
 import './styles.css'
 
 export default function BarChart(props) {
+    const [position, setPosition] = useState(false)
 
     const {
         title,
@@ -11,6 +13,7 @@ export default function BarChart(props) {
 
     const options = {
         maintainAspectRatio: false,
+        indexAxis: position ? 'y' : 'x',
         plugins: {
             legend: {
                 display: false
@@ -33,6 +36,7 @@ export default function BarChart(props) {
     return (
         <div className='barchart-container' style={{ width: barWidth, height: barHeight }}>
             <h4 className='table-title'>{title || ''}</h4>
+            <img src={rotateIcon} className='chart-rotate-svg' onClick={() => setPosition(!position)} />
             <Bar data={chartData} height={barHeight} width={barWidth} options={options} />
         </div>
     )
