1a648ecd879f78989f488f29570deaa8e5a265f3
Guillermo Sotelo <guillermo.sotelo@conexiogroup.com>
2023-02-07 20:45:25 +0100

    updated landing dark mode

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -10,6 +10,10 @@ export default function Landing() {
     const isMobile = window.innerWidth < 640
     const history = useHistory()
 
+    useEffect(() => {
+        const mode = localStorage.getItem('darkMode') ? JSON.parse(localStorage.getItem('darkMode')) : false
+        setDarkMode(mode)
+    }, [])
     // useEffect(() => {
     //     const localUser = JSON.parse(localStorage.getItem('user'))
 
@@ -35,12 +39,15 @@ export default function Landing() {
                     <a></a>
                 </div>
                 <div className='landing-header-btns'>
-                    <img 
-                    src={MoonSon} 
-                    alt='Dark Mode' 
-                    className='landing-dark-mode' 
-                    onClick={() => setDarkMode(!darkMode)} 
-                    style={{ filter: darkMode ? 'invert(97%) sepia(0%) saturate(4674%) hue-rotate(353deg) brightness(80%) contrast(99%)' : ''}}
+                    <img
+                        src={MoonSon}
+                        alt='Dark Mode'
+                        className='landing-dark-mode'
+                        onClick={() => {
+                            setDarkMode(!darkMode)
+                            localStorage.setItem('darkMode', !darkMode)
+                        }}
+                        style={{ filter: darkMode ? 'invert(97%) sepia(0%) saturate(4674%) hue-rotate(353deg) brightness(80%) contrast(99%)' : '' }}
                     />
                     <CTAButton
                         label='Login'
@@ -60,8 +67,8 @@ export default function Landing() {
             </div>
             <div className='landing-body'>
                 <div className='landing-text-div'>
-                    <h1 className='landing-heading'>Empowering your financial control<br /> with CtrlShift</h1>
-                    <p className='landing-text'>
+                    <h1 className='landing-heading move-x-back'>Empowering your financial control<br /> with CtrlShift</h1>
+                    <p className='landing-text move-x-back'>
                         <b>CtrlShift</b> is a comprehensive finance management app designed for homes and small businesses.<br />
                         With it, you can easily submit expenses, track income and outcome, and have full control
                         over your finances with the help of graphics and other useful modules.<br /><br />
@@ -69,14 +76,14 @@ export default function Landing() {
                     </p>
                     <CTAButton
                         label='Start for free'
-                        className='landing-cta'
+                        className='landing-cta move-y-back'
                         color='#CCA43B'
                         style={{ color: 'black' }}
                         handleClick={() => history.push('/register')}
                     />
                 </div>
                 <div className='landing-phone-div'>
-                    <img src='https://i.postimg.cc/GtVz8x3L/smartmockups-ldue73hz.png' alt='mobile app image' className='landing-phone-image' />
+                    <img src='https://i.postimg.cc/GtVz8x3L/smartmockups-ldue73hz.png' alt='mobile app image' className='landing-phone-image move-x' />
                 </div>
             </div>
         </div>
--- src/pages/LandingMobile.js
+++ src/pages/LandingMobile.js
@@ -1,12 +1,20 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useHistory } from 'react-router-dom'
 import CTAButton from '../components/CTAButton'
 import Logo from '../assets/logo.png'
 import Footer from '../components/Footer'
 import MoonSon from '../assets/moon-sun.svg'
+import Landing from './Landing'
 
 export default function LandingMobile() {
+    const [darkMode, setDarkMode] = useState(false)
+    const isMobile = window.innerWidth < 640
     const history = useHistory()
+
+    useEffect(() => {
+        const mode = localStorage.getItem('darkMode') ? JSON.parse(localStorage.getItem('darkMode')) : false
+        setDarkMode(mode)
+    }, [])
 
     // useEffect(() => {
     //     const localUser = JSON.parse(localStorage.getItem('user'))
@@ -24,20 +32,27 @@ export default function LandingMobile() {
     //     if (localUser && localUser.token && localUser.app && localUser.app === 'ctrl-shift') return history.push('/home')
     // }, [])
 
-    return (
-        <div className='landing-mobile-container'>
-            <div className='landing-header'>
+    return isMobile ?
+        <div className='landing-mobile-container' style={{ backgroundColor: darkMode ? '#202020' : '', color: darkMode ? 'lightgray' : '' }}>
+            <div className='landing-header move-y'>
                 <img className='landing-header-logo' src={Logo} alt="Ctrol Shiflt" />
                 <div className='landing-header-central'>
                     <a></a>
                 </div>
                 <div className='landing-header-btns'>
-                <img src={MoonSon} alt='Dark Mode' className='landing-dark-mode' />
-                    <CTAButton
+                    <img
+                        src={MoonSon}
+                        alt='Dark Mode'
+                        className='landing-dark-mode'
+                        onClick={() => {
+                            setDarkMode(!darkMode)
+                            localStorage.setItem('darkMode', !darkMode)
+                        }} style={{ filter: darkMode ? 'invert(97%) sepia(0%) saturate(4674%) hue-rotate(353deg) brightness(80%) contrast(99%)' : '' }}
+                    />                    <CTAButton
                         label='Login'
                         className='landing-cta'
-                        color='#fff'
-                        style={{ color: 'black' }}
+                        color='transparent'
+                        style={{ color: darkMode ? 'lightgray' : 'black' }}
                         handleClick={() => history.push('/login')}
                     />
                     <CTAButton
@@ -51,24 +66,27 @@ export default function LandingMobile() {
             </div>
             <div className='landing-body'>
                 <div className='landing-text-div'>
-                    <h1 className='landing-heading'>Empowering your financial control with CtrlShift</h1>
-                    <p className='landing-text'>
+                    <h1 className='landing-heading move-x'>Empowering your financial control with CtrlShift</h1>
+                    <CTAButton
+                        label='Start for free'
+                        className='landing-cta-scale move-x-back'
+                        color='#CCA43B'
+                        style={{ color: 'black', transform: 'scale(1.3)' }}
+                        handleClick={() => history.push('/register')}
+                    />
+                </div>
+                <img src='https://i.postimg.cc/GtVz8x3L/smartmockups-ldue73hz.png' alt='mobile app image' className='landing-phone-image move-y-back' />
+                <div className='landing-text-div2'>
+                    <p className='landing-text move-x-back'>
                         <b>CtrlShift</b> is a comprehensive finance management app designed for homes and small businesses.<br />
                         With it, you can easily submit expenses, track income and outcome, and have full control
                         over your finances with the help of graphics and other useful modules.<br /><br />
                         Simplify your financial management with <b>CtrlShift</b>.
                     </p>
-                    <CTAButton
-                        label='Start for free'
-                        className='landing-cta-scale'
-                        color='#CCA43B'
-                        style={{ color: 'black' }}
-                        handleClick={() => history.push('/register')}
-                    />
                 </div>
-                <img src='https://i.postimg.cc/GtVz8x3L/smartmockups-ldue73hz.png' alt='mobile app image' className='landing-phone-image' />
             </div>
             <Footer />
         </div>
-    )
+        :
+        <Landing />
 }
